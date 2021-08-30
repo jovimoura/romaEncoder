03a06cf048a157036e641283bba75a7e86000379
jovimoura <joaovictormoura@poli.ufrj.br>
2021-08-30 15:46:18 -0300

    alterações base64 concluido

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Escrever o texto
 
-let texto = document.getElementById('texto');
+let textoEntrada = document.getElementById('texto-entrada');
+let textoSaida = document.getElementById('texto-saida')
 
 //Botao selecionar criptografia
 let cesar = document.getElementById('cesar');
@@ -14,30 +15,50 @@ let decodBtn = document.getElementById('decodificar-btn');
 codeBtn.classList.add('none');
 decodBtn.classList.add('none');
 
-//Depois de selecionar tipo de criptografia, escolher entre codificar ou decodificar
-
-
-/* Entregar mensagem codificada/decodificada no textarea de leitura*/
-
-
+let botaoApagar = document.getElementById('btn-apagar');
 
                             //base64
 
 codificar.addEventListener('change',()=>{
+    decodBtn.classList.add('none');
     if(codificar.checked === true){
-    codeBtn.classList.remove();
-}
+        codeBtn.classList.remove('none');
+    }
 });
 
 decodificar.addEventListener('change',()=>{
+    codeBtn.classList.add('none');
     if(decodificar.checked === true){
-    decodBtn.classList.remove();
-}
+        decodBtn.classList.remove('none');
+    }
 });
 
 codeBtn.addEventListener('click',()=>{
-    
+    if(selecao.value === 'base64'){
+        textoSaida.value = btoa(textoEntrada.value);
+    }else{
+        console.log('cesarC');
+    }
+        
+});
+
+decodBtn.addEventListener('click',()=>{
+    if(selecao.value === 'base64'){
+        textoSaida.value = atob(textoEntrada.value)
+    }else{
+        console.log('cesarD');
+    }
+        
+});
+
+botaoApagar.addEventListener('click',()=>{
+    textoSaida.value = '';
 })
+
+
+
+
+
 
 
 
